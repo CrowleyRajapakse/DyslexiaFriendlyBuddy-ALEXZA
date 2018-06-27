0fb7ede873492711f03b70c7296dc8ab2bf9ddec
Crowley Rajapakse <srrajapakse1@gmail.com>
2018-06-27 11:23:56 +0530

    Revert "Revert "adding alexza bot""
    This reverts commit 59d98d02c9538d0cbea381488b365bf7c48f7acd.

--- app/src/main/java/com/fsociety2/dyslexiafriendlybuddy/MainActivity.java
+++ app/src/main/java/com/fsociety2/dyslexiafriendlybuddy/MainActivity.java
@@ -31,7 +31,7 @@ public class MainActivity extends AppCompatActivity
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), ContactUs.class);
+                Intent intent = new Intent(getApplicationContext(), ChatBotActivity.class);
                 startActivity(intent);
             }
         });
